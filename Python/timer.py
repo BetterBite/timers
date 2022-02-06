f7bedfb4c9ca2cdfe0097ae4aca30e8211ed871e
BetterBite <gabrielgaladyk@yahoo.pl>
2022-02-06 22:33:45 +0000

    Updated timer.py

--- Python/timer.py
+++ Python/timer.py
@@ -21,22 +21,22 @@ class Countup:
         time.sleep(1)
 
     def print(self):
-        print("%02d:%02d:%02d "+str(self.secs)+"s")
+        print(str(self.z).rjust(2, '0')+":"+str(self.y).rjust(2, '0')+":"+str(self.x).rjust(2, '0')+" "+str(self.secs)+"s")
 
 #https://youtu.be/M2dhD9zR6hk
 class Countdown:
     x = 0
     y = 0
     z = 0
-    secs = 0
+    secs = 1
 
     def __init__(self, amount, mins):
         if mins == True:
             self.x = 1
-            #do something
+            #TO DO
         else:
             self.secs = amount
-
+            #proper assignment of time variables
             self.z = self.secs//3600
             self.secs -= self.z*3600
             
@@ -44,42 +44,75 @@ class Countdown:
             self.secs -= self.y*60
 
             self.x = self.secs
-            self.secs = amount 
+            self.secs = amount #reset to original value
+
+    def decrement(self):
+        self.secs -= 1
+        self.x -= 1
+        if self.x < 0:
+            self.x = 59
+            self.y -= 1
+        if self.y < 0:
+            self.y = 59
+            self.z -= 1
+        time.sleep(1)
+
+    def print(self):
+        print(str(self.z).rjust(2, '0')+":"+str(self.y).rjust(2, '0')+":"+str(self.x).rjust(2, '0')+" "+str(self.secs)+"s")
 
 
 #Main, technically main, code execution starts here
-while True: #two while loops permit a kind of retry should an input be invalid
+while True: #while True: represents a block with input where upon rejection of input will prompt user to retry through continue
+    choice = input("Select mode, 'countup' for counting up, 'countdown' for counting down: ")
+    if choice != "countup" and choice != "countdown":
+        print("Invalid input. Try again.")
+        continue
+    break
+
+
+if choice == "countup":
+    clock = Countup()
+    while clock.secs != 360000: #100 hour limit
+        clock.increment()
+        clock.print()
+    if clock.secs == 360000:
+        time.sleep(1)
+        print("Counting finished, limit of 100h reached")
+        exit(0)
+    elif clock.secs != 360000:
+        time.sleep(1)
+        print("Counting finished, early termination")
+        exit(0)
+
+
+if choice == "countdown":
+    mode = False
     while True:
-        choice = input("Select mode, 'countup' for counting up, 'countdown' for counting down")
-        if choice != "countup" or choice != "countdown":
-            print("Invalid input. Try again.")
+        amount = input("Insert time in seconds, or type 'm' to insert time in minutes. ")
+        if amount == 'm':
+            mode = True
+            while True: #in minutes
+                amount = input("Insert time in minutes. ")
+                try:
+                    amount = int(amount)
+                except:
+                    print("Invalid input.")
+                    continue
+                break
+        try:
+            amount = int(amount)
+        except:
+            print("Invalid input.")
             continue
         break
-    if choice == "countup":
-        clock = Countup()
-        while clock.secs != 360000: #100 hour limit
-            clock.increment()
-            clock.print()
-        if clock.secs == 360000:
-            print("Counting finished, limit of 100h reached")
-            exit(0)
-        elif clock.secs != 360000:
-            print("Counting finished, early termination")
-            exit(0)
-    if choice == "countdown":
-        while True:
-            amount = input("Insert time in seconds, or type 'm' to insert time in minutes.")
-            if amount == 'm':
-                mode = True
-                while True: #in minutes
-                    amount = input("Insert time in minutes.")
-                    if isinstance(amount, int) == True:
-                        break
-                    continue
-            elif isinstance(amount, int) == True: #in seconds
-                break
-            else:
-                print("Invalid input.")
-                continue
-            break
-        #call a class
+    clock = Countdown(amount, mode)
+    while clock.secs != 0:
+        clock.decrement()
+        clock.print()
+    if clock.secs == 0:
+        time.sleep(1)
+        print("Finished counting.")
+    elif clock.secs != 0:
+        time.sleep(1)
+        print("Finished counting, early termination")
+    
