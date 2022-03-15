4a85a9272ad631d63ea1cc58bf41a2f0d584851f
BetterBite <38665746+BetterBite@users.noreply.github.com>
2022-03-15 16:52:40 +0000

    Standardized timer.py

--- Python/timer.py
+++ Python/timer.py
@@ -1,7 +1,8 @@
 import time
 
-#field of constants
-TIME_LIMIT = 360000 #100h time limit
+# field of constants
+TIME_LIMIT = 360000  # 100h time limit
+
 
 class Countup:
     x = 0
@@ -21,9 +22,11 @@ class Countup:
         time.sleep(1)
 
     def print(self):
-        print(str(self.z).rjust(2, '0')+":"+str(self.y).rjust(2, '0')+":"+str(self.x).rjust(2, '0')+" "+str(self.secs)+"s")
+        print(str(self.z).rjust(2, '0') + ":" + str(self.y).rjust(2, '0') + ":" + str(self.x).rjust(2, '0') + " " + str(
+            self.secs) + "s")
 
-#https://youtu.be/M2dhD9zR6hk
+
+# https://youtu.be/M2dhD9zR6hk
 class Countdown:
     x = 0
     y = 0
@@ -31,23 +34,23 @@ class Countdown:
     secs = 1
 
     def __init__(self, amount, mins):
-        if mins == True:
-            self.secs = amount*60
+        if mins:
+            self.secs = amount * 60
             self.timeassignment()
-            self.secs = amount*60
+            self.secs = amount * 60
         else:
             self.secs = amount
             self.timeassignment()
-            self.secs = amount #both paths manipulate self.secs as timeassignment performs its operations and thus it must be reset upon completion
-
+            self.secs = amount  # both paths manipulate self.secs as timeassignment performs its operations and thus
+            # it must be reset upon completion
 
     def timeassignment(self):
-        #proper assignment of time variables
-        self.z = self.secs//3600
-        self.secs -= self.z*3600
-        
-        self.y = self.secs//60
-        self.secs -= self.y*60
+        # proper assignment of time variables
+        self.z = self.secs // 3600
+        self.secs -= self.z * 3600
+
+        self.y = self.secs // 60
+        self.secs -= self.y * 60
 
         self.x = self.secs
 
@@ -63,21 +66,22 @@ class Countdown:
         time.sleep(1)
 
     def print(self):
-        print(str(self.z).rjust(2, '0')+":"+str(self.y).rjust(2, '0')+":"+str(self.x).rjust(2, '0')+" "+str(self.secs)+"s")
+        print(str(self.z).rjust(2, '0') + ":" + str(self.y).rjust(2, '0') + ":" + str(self.x).rjust(2, '0') + " " + str(
+            self.secs) + "s")
 
 
-#Main, technically main, code execution starts here
-while True: #while True: represents a block with input where upon rejection of input will prompt user to retry through continue
+# Main, technically main, code execution starts here
+while True:  # while True: represents a block with input whereupon rejection of input will prompt user to retry
+    # through continue
     choice = input("Select mode, 'countup' for counting up, 'countdown' for counting down: ")
     if choice != "countup" and choice != "countdown":
         print("Invalid input. Try again.")
         continue
     break
 
-
 if choice == "countup":
     clock = Countup()
-    while clock.secs != 360000: #100 hour limit
+    while clock.secs != 360000:  # 100 hour limit
         clock.increment()
         clock.print()
     if clock.secs == 360000:
@@ -89,27 +93,24 @@ if choice == "countup":
         print("Counting finished, early termination")
         exit(0)
 
-
 if choice == "countdown":
     mode = False
     while True:
         amount = input("Insert time in seconds, or type 'm' to insert time in minutes. ")
         if amount == 'm':
             mode = True
-            while True: #in minutes
+            while True:  # in minutes
                 amount = input("Insert time in minutes. ")
-                try:
-                    amount = int(amount)
-                except:
+                if isinstance(amount, int):
+                    break
+                else:
                     print("Invalid input.")
                     continue
-                break
-        try:
-            amount = int(amount)
-        except:
+        if isinstance(amount, int):
+            break
+        else:
             print("Invalid input.")
             continue
-        break
     clock = Countdown(amount, mode)
     while clock.secs != 0:
         clock.decrement()
@@ -120,4 +121,3 @@ if choice == "countdown":
     elif clock.secs != 0:
         time.sleep(1)
         print("Finished counting, early termination")
-    
